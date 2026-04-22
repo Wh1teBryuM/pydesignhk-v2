62f13e6c27cbd3495d3a1aaa47fbe10edbd2dfd8
Wh1teBryuM <bn762004@gmail.com>
2026-04-22 23:49:25 +0800

    update in route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { registerCustomer, loginCustomer } = require('../controllers/authController')
+const { registerCustomer, loginCustomer, logoutCustomer } = require('../controllers/authController')
 
 router.post('/register', registerCustomer)
 router.post('/login', loginCustomer)
+router.post('/logout', logoutCustomer)
 
 module.exports = router
